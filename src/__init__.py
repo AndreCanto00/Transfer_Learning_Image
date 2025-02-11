9215cba8ab01f73a018868601b2ae99b19f42afc
AndreCanto00 <112171091+AndreCanto00@users.noreply.github.com>
2025-02-11 20:13:42 +0000

    Fix

--- src/__init__.py
+++ src/__init__.py
@@ -5,13 +5,13 @@ from . import utils
 __version__ = '0.1.0'
 
 # src/data/__init__.py
-from .data_loader import create_data_loaders
-from .data_preprocessing import split_dataset
+from data.data_loader import create_data_loaders
+from data.data_preprocessing import split_dataset
 
 # src/models/__init__.py
-from .model import ModelBuilder
-from .training import ModelTrainer, hyperparameter_search
-from .evaluation import ModelEvaluator, evaluate_and_save_model
+from models.model import ModelBuilder
+from models.training import ModelTrainer, hyperparameter_search
+from models.evaluation import ModelEvaluator, evaluate_and_save_model
 
 # src/utils/__init__.py
-from .file_operations import ensure_directory, get_class_directories, organize_files
+from utils.file_operations import ensure_directory, get_class_directories, organize_files
